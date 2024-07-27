90c9f1ea87773badb184b5d020f178eb853fc225
Sukharevich Dmitry <defgolem@gmail.com>
2024-07-27 15:35:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,34 +33,33 @@ function sendRequestToPipedriveAPI(deal) {
 }
 
 window.addEventListener('message', function(event) {
-    // Проверяем источник сообщения
     if (event.origin !== 'https://sukharevichdmitry.github.io') return;
 
-    // Проверяем, было ли уже создано дело
     if (localStorage.getItem(LOCAL_STORAGE_KEY) === 'true') {
         console.log('Дело уже создано.');
         return; 
     }
-
-    var inputInfo = event.data;
-    console.log('Полученные данные:', inputInfo);
-
-    var pipedriveDeal = {
-        title: 'Job from iframe',
-        '4c4d96e6eeba926553699dffbb865509da63f59f': inputInfo.address,
-        'bd2880ef1545ad3a87214ba775cbf8cf475a940c': inputInfo.jobType,
-        '7b7a9026f79c39c94e0231c8b9f22833e30e5e94': inputInfo.scheduleDate,
-        '7379a510b4d98e6a4d25c9b4a9801b1bccb38d01': inputInfo.startTime,
-        'd91cf0b42a8bcbd8ec7400b0e278e787ea19cf5e': inputInfo.endTime,
-        'a287c45e151cefe1f690b800b034921467ba1c4d': inputInfo.area,
-        'aaa68fb0dc680d9dab59cbd3bfe105a25aa3cbdd': inputInfo.area,
-        'eddcc194169f6cf2340bf86c6973a481881218dd': inputInfo.jobDetails,
-    };
-
-    // Устанавливаем флаг, чтобы предотвратить повторное создание дела
-    localStorage.setItem(LOCAL_STORAGE_KEY, 'true');
+    else {
+        var inputInfo = event.data;
+        console.log('Полученные данные:', inputInfo);
     
-    sendRequestToPipedriveAPI(pipedriveDeal);
+        var pipedriveDeal = {
+            title: 'Job from iframe',
+            '4c4d96e6eeba926553699dffbb865509da63f59f': inputInfo.address,
+            'bd2880ef1545ad3a87214ba775cbf8cf475a940c': inputInfo.jobType,
+            '7b7a9026f79c39c94e0231c8b9f22833e30e5e94': inputInfo.scheduleDate,
+            '7379a510b4d98e6a4d25c9b4a9801b1bccb38d01': inputInfo.startTime,
+            'd91cf0b42a8bcbd8ec7400b0e278e787ea19cf5e': inputInfo.endTime,
+            'a287c45e151cefe1f690b800b034921467ba1c4d': inputInfo.area,
+            'aaa68fb0dc680d9dab59cbd3bfe105a25aa3cbdd': inputInfo.area,
+            'eddcc194169f6cf2340bf86c6973a481881218dd': inputInfo.jobDetails,
+        };
+        
+        sendRequestToPipedriveAPI(pipedriveDeal);
+        localStorage.setItem(LOCAL_STORAGE_KEY, 'true');
+    }    
+
+    
 });
 
 window.onload = function () {
